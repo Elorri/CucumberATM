0e5a47fa97ef22af64133a3a7a6aff452b1b776d
Elorri <etchemendy.elorri@gmail.com>
2016-06-24 22:43:30 +0200

    Tidy: KnowsTheDomain in its own class

---
+++ app/src/test/java/com/example/android/cucumberatm/KnowsTheDomain.java
@@ -0,0 +1,34 @@
+package com.example.android.cucumberatm;
+
+import android.accounts.Account;
+
+/**
+ * Created by Elorri on 24/06/2016.
+ */
+
+class KnowsTheDomain {
+    private Account myAccount;
+    private CashSlot cashSlot;
+    private Teller teller;
+
+    public Account getMyAccount() {
+        if (myAccount == null){
+            myAccount = new Account();
+        }
+        return myAccount;
+    }
+    public CashSlot getCashSlot() {
+        if (cashSlot == null){
+            cashSlot = new CashSlot();
+        }
+        return cashSlot;
+    }
+
+    public Teller getTeller() {
+        if (teller == null){
+            teller = new Teller(getCashSlot());
+        }
+        return teller;
+    }
+
+}
--- app/src/test/java/com/example/android/cucumberatm/Steps.java
+++ app/src/test/java/com/example/android/cucumberatm/Steps.java
@@ -37,32 +37,7 @@ public class Steps {
 
 
 
-    class KnowsTheDomain {
-        private Account myAccount;
-        private CashSlot cashSlot;
-        private Teller teller;
 
-        public Account getMyAccount() {
-            if (myAccount == null){
-                myAccount = new Account();
-            }
-            return myAccount;
-        }
-        public CashSlot getCashSlot() {
-            if (cashSlot == null){
-                cashSlot = new CashSlot();
-            }
-            return cashSlot;
-        }
-
-        public Teller getTeller() {
-            if (teller == null){
-                teller = new Teller(getCashSlot());
-            }
-            return teller;
-        }
-
-    }
 
 
 
