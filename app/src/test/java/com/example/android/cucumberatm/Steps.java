562f1cff3f191ff6bdf3aa8ab9d8e7d988a6cd44
Elorri <etchemendy.elorri@gmail.com>
2016-06-24 22:24:10 +0200

    Add dependency injection

--- app/src/test/java/com/example/android/cucumberatm/Steps.java
+++ app/src/test/java/com/example/android/cucumberatm/Steps.java
@@ -59,7 +59,12 @@ public class Steps {
 
 
     class Teller {
+        private CashSlot cashSlot;
+        public Teller(CashSlot cashSlot) {
+            this.cashSlot = cashSlot;
+        }
         public void withdrawFrom(Account account, int dollars) {
+            cashSlot.dispense(dollars);
         }
     }
 
